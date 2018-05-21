e3f3cd6cc2c748f2ae871340597be528b384178c
Dobrin Yonkov <dobrin.yonkov1@gmail.com>
2018-05-21 12:31:36 +0300

    two-way data binding

--- basics/reactjs/src/componetns/Home.js
+++ basics/reactjs/src/componetns/Home.js
@@ -1,18 +1,60 @@
 import React from 'react'
 
 export class Home extends React.Component {
-    render () {
-        var user = this.props.user;
-        console.log(this.props)
+    
+    constructor(props) {
+        super();
+        this.state = {
+            user: props.user,
+            greet: props.greet,
+            initialName: props.initialName
+        };
+    }
+
+    onMakeHerOlder() {
+        this.setState({
+            user: {
+                ...this.state.user,
+                age: this.state.user.age + 3
+            }
+        })
+        console.log(this.state.user);
+    }
+
+    onChangeLink() {
+        console.log(this.state.initialName)
+        this.props.changeLinkName(this.state.initialName)
+    }
+
+    onChangeName(event) {
+        var newName = event.target.value
+        this.setState({
+            initialName: newName
+        })
+    }
+
+    render() {
         return (
             <div>
-                <p>Hello {user.name}</p>
-                <p>age: {user.age}</p>
+                <p>Hello {this.state.user.name}</p>
+                <p>age: {this.state.user.age}</p>
                 <ul>
-                    {user.hobbies.map(element => <li>{element}</li>)}
+                    {this.state.user.hobbies.map((element, index) => <li key={index}>{element}</li>)}
                 </ul>
-
+                <hr />
+                {this.props.children}
+                <button onClick={() => this.onMakeHerOlder()} className="btn btn-primary">Make her older</button>
+                <button onClick={this.state.greet.bind(this)} className="btn btn-primary ml-3">Greet</button>
+                {/* <button onClick={this.onMakeHerOlder.bind(this)} className="btn btn-primary">Make her older</button> */}
+                <hr/>
+                <input
+                    className="form-control" 
+                    value={this.state.initialName} 
+                    onChange={(event) => {this.onChangeName(event)}}/>
+                <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Name</button>
+                
             </div>
         );
     }
 }
+
--- basics/reactjs/src/index.js
+++ basics/reactjs/src/index.js
@@ -5,23 +5,65 @@ import { Header } from './componetns/Header'
 import { Home } from './componetns/Home'
 
 class App extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            name: 'Dobrin',
+            user: {
+                name: 'Ana',
+                age: 27,
+                hobbies: [
+                    "runnig",
+                    "coding",
+                    'eating'
+                ]
+            }
+        }
+    }
+
+
+    onGreet() {
+        console.log(this);
+        this.setState({
+            user: {
+                ...this.state.user,
+                age: this.state.user.age + 3
+            }
+        })
+        console.log(this.state.user);
+    }
+
+    onChangeLinkName(newName) {
+        console.log(newName)
+        console.log(this);
+        this.setState({
+            name: newName
+        })
+    }
+
     render() {
 
-        var user = {
-            name: 'Ana',
-            age: 27,
-            hobbies : [
-                "runnig",
-                "coding",
-                'eating'
-            ]
-        }
-        
         return (
             <div>
-                <Header user={user}/>
+                <div className="row">
+                    <div className="col">
+                        <Header name={this.state.name} />
+                    </div>
+                </div>
 
-                <Home user={user}/>
+                <div className="row">
+                    <div className="col">
+                        <Home
+                            user={this.state.user} 
+                            greet={this.onGreet} 
+                            changeLinkName={this.onChangeLinkName.bind(this)}
+                            initialName={this.state.name}>
+                            <p>I'm a child paragraph</p>
+                        </Home>
+                    </div>
+                </div>
             </div>
         );
     }
