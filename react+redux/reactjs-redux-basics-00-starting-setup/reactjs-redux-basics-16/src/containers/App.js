f47a1cd685b6cf395cc80530392ee4fa2aa6b460
Dobrin Yonkov <dobrin.yonkov1@gmail.com>
2018-05-22 13:19:12 +0300

    change to a better project structure

--- react+redux/reactjs-redux-basics-00-starting-setup/reactjs-redux-basics-16/src/containers/App.js
+++ react+redux/reactjs-redux-basics-00-starting-setup/reactjs-redux-basics-16/src/containers/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux'
 
-import { User } from './User';
-import { Main } from './Main';
+import { User } from '../components/User';
+import { Main } from '../components/Main';
+
+import {setName} from '../actions/userAction'
 
 class App extends React.Component {
     render() {
@@ -25,10 +27,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         setName: (name) => {
-            dispatch({
-                type: 'RENAME',
-                payload: name
-            });
+            dispatch(setName(name));
         }
     };
 }
--- react+redux/reactjs-redux-basics-00-starting-setup/reactjs-redux-basics-16/src/index.js
+++ react+redux/reactjs-redux-basics-00-starting-setup/reactjs-redux-basics-16/src/index.js
@@ -1,75 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { logger } from 'redux-logger';
+
 import { Provider } from 'react-redux';
 
-import App from './components/App';
+import App from './containers/App';
 
-const mathReducer = function (state = {
-    result: 1,
-    lastValues: []
-}, action) {
-    switch (action.type) {
-        case "ADD":
-            state = {
-                ...state,
-                result: state.result + action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            }
-            break;
-        case "SUBSTRACT":
-            state = {
-                ...state,
-                result: state.result - action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            }
-            break;
-        case "MULTIPLY":
-            state = {
-                ...state,
-                result: state.result * action.payload,
-                lastValues: [...state.lastValues, action.payload]
-            }
-            break;
+import store from './store'
 
-        default:
-            break;
-    }
-
-    return state;
-}
-
-const userReducer = function (state = {
-    name: "Bob",
-    age: 22
-}, action) {
-    switch (action.type) {
-        case "RENAME":
-            state = {
-                ...state,
-                name: action.payload,
-            }
-            break;
-        case "CHANGE_AGE":
-            state = {
-                ...state,
-                age: action.payload,
-            }
-            break;
-
-        default:
-            break;
-    }
-
-    return state;
-}
-
-const store = createStore(
-    combineReducers({ math: mathReducer, user: userReducer }),
-    {},
-    applyMiddleware(logger)
-);
 
 ReactDOM.render(
     <Provider store={store}>
