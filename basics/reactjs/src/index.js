3d83f097b68408ba62f8a0357208ad33a3239789
Dobrin Yonkov <dobrin.yonkov1@gmail.com>
2018-05-22 12:49:01 +0300

    adding redux, learning reducers, store, actions, middlewares,connecting react and redux

--- basics/reactjs/src/index.js
+++ basics/reactjs/src/index.js
@@ -1,73 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
 
-import { Header } from './componetns/Header'
-import { Home } from './componetns/Home'
+import { Root } from "./components/Root";
+import { Home } from "./components/Home";
+import { User } from "./components/User";
 
 class App extends React.Component {
-
-    constructor() {
-        super();
-
-        this.state = {
-            name: 'Dobrin',
-            user: {
-                name: 'Ana',
-                age: 27,
-                hobbies: [
-                    "runnig",
-                    "coding",
-                    'eating'
-                ]
-            }
-        }
-    }
-
-
-    onGreet() {
-        console.log(this);
-        this.setState({
-            user: {
-                ...this.state.user,
-                age: this.state.user.age + 3
-            }
-        })
-        console.log(this.state.user);
-    }
-
-    onChangeLinkName(newName) {
-        console.log(newName)
-        console.log(this);
-        this.setState({
-            name: newName
-        })
-    }
-
     render() {
-
         return (
-            <div>
-                <div className="row">
-                    <div className="col">
-                        <Header name={this.state.name} />
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col">
-                        <Home
-                            user={this.state.user} 
-                            greet={this.onGreet} 
-                            changeLinkName={this.onChangeLinkName.bind(this)}
-                            initialName={this.state.name}>
-                            <p>I'm a child paragraph</p>
-                        </Home>
-                    </div>
-                </div>
-            </div>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path={"/"} component={Root}>
+                        <Route path={"/user"} component={User} />
+                        <Route path={"/home"} component={Home} />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
